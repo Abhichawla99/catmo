c5f33a55b6842c05342102c23c941c3755c753e6
Abhichawla99 <abhixchawla@gmail.com>
2025-12-05 22:28:41 -0600

    Add Vercel Analytics to track page views and user interactions
    - Installed @vercel/analytics package
    - Added Analytics component to App.tsx
    - Will automatically track page views and user interactions when deployed on Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import { Navbar } from './components/layout/Navbar';
 import { Footer } from './components/layout/Footer';
 import { HomePage } from './pages/HomePage';
@@ -226,6 +227,7 @@ function App() {
           </Routes>
         </main>
         <Footer />
+        <Analytics />
       </div>
     </Router>
   )
